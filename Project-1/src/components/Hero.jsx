80b193307f3265086d4794b6e7f03d70fa247a7a
nazibhossen <nazibhossen1.com>
2023-07-30 02:55:44 +0600

    Minore change

--- Project-1/src/components/Hero.jsx
+++ Project-1/src/components/Hero.jsx
@@ -4,7 +4,6 @@ const HeroSection = () => {
         <div className="hero-content">
             <h1>YOUR FEET DESERVE THE BEST</h1>
             <p>YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.</p>
-
             <div className="hero-btn">
                 <button>Shop Now</button>
                 <button className="secondary-btn">Category</button>
@@ -20,7 +19,6 @@ const HeroSection = () => {
         <div className="hero-image">
             <img src="/images/shoe_image.png" alt="hero-image" />
         </div>
-
     </main>
     );
 };
--- Project-1/src/components/Navigation.jsx
+++ Project-1/src/components/Navigation.jsx
@@ -5,12 +5,11 @@
           <img src="/images/brand_logo.png" alt="logo" />
         </div>
         <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Location</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
-        
         <button>Login</button>
       </nav>
     );
